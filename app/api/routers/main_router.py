46d13ab895e43162eddf48b97dd9ac46e41524e1
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2026-04-19 21:09:47 +0600

    feat: add project working indicator

--- app/api/routers/main_router.py
+++ app/api/routers/main_router.py
@@ -3,6 +3,10 @@ from app.api.routers.user import user_router
 
 router = APIRouter()
 
+@router.get("/")
+def root():
+    return {"message": "Welcome to FastAPI Production Boilerplate!"}
+
 router.include_router(user_router)
 
 
